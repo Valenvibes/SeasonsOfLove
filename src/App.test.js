aa532b65ed422ece1abb61f63e214d5639a1f97e
Valenvibes <valenvibes.team@gmail.com>
2025-02-08 17:53:31 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders start page correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/มาตามหาตัวตนกัน ว่าคุณเป็นหมาหรือแมวสายพันธุ์ใด/i);
+  const buttonElement = screen.getByText(/ไปกันเลยยย/i);
+  expect(headingElement).toBeInTheDocument();
+  expect(buttonElement).toBeInTheDocument();
 });
