349c0b9b565f2b8775ebac1069a42afbee56987e
Valenvibes <valenvibes.team@gmail.com>
2025-02-08 18:03:27 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders start page correctly', async () => {
   render(<App />);
-  const headingElement = await screen.findByText(/มาตามหาตัวตนกัน ว่าคุณเป็นหมาหรือแมวสายพันธุ์ใด/i);
+  const headingElement = await screen.findByText(/มาตามหาตัวตนกัน ว่าคุณเป็นหมาหรือแมวสายพันธ์ุใด?/i);
   const buttonElement = await screen.findByText(/ไปกันเลยยยย/i);
   expect(headingElement).toBeInTheDocument();
   expect(buttonElement).toBeInTheDocument();
